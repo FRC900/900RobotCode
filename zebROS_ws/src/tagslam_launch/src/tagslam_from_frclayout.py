a211515fd5a4bfae5e68d9a090166ae80950e2ae
Torchtopher <torchtopher@gmail.com>
2025-01-06 17:54:58 -0500

    add reefscape field model

--- zebROS_ws/src/tagslam_launch/src/tagslam_from_frclayout.py
+++ zebROS_ws/src/tagslam_launch/src/tagslam_from_frclayout.py
@@ -1,45 +1,6 @@
 from scipy.spatial.transform import Rotation
 import yaml
 
-# data = dict(
-#     tags = [
-#         dict(
-#             id = 1,
-#             size = 0.165
-#         ),
-#         dict(
-#             id = 2,
-#             size = 0.165
-#         )
-#     ]
-# )
-
-# with open('data.yml', 'w') as outfile:
-#     yaml.dump(data, outfile, default_flow_style=False)
-# '''     
-# tags:
-# - id: 7
-#     size: 0.16510000
-#     pose:
-#     position:
-#         x: -0.01270000
-#         y: 5.54786800
-#         z: 1.45110200
-#     rotation:
-#         x: 1.20919970
-#         y: 1.20919970
-#         z: 1.20920010
-#     position_noise:
-#         x: 0.00010000
-#         y: 0.00010000
-#         z: 0.00010000
-#     rotation_noise:
-#         x: 0.00001000
-#         y: 0.00001000
-#         z: 0.00001000
-# '''
-# # multi cursor magic > regex
-
 # x y z angle id
 tags = [
 
@@ -54,9 +15,12 @@ lines.pop()
 for tag in lines:
     
     # there format is, [id, x, y, z, z rot, y rot 
-    # our format is    [x, y, z, y rot, id
+    # our format is    [x, y, z, z_rot, y rot, id
+
+    # their Z rot is our Y rot 
+    # their X rot is our... Z rot 
     # no z rot for now
-    tag_id, x, y, z, z_rot, y_rot = tag.split(",")
+    tag_id, x, y, z, y_rot, z_rot = tag.split(",")
     tag_id = int(tag_id)
     x = float(x)
     y = float(y)
@@ -69,7 +33,7 @@ for tag in lines:
     z *= 0.0254
     print(f"tag id {tag_id}, x {x}, y {y}, {z}, z_rot {z_rot}, y rot {y_rot}")
 
-    tags.append([x, y, z, y_rot, tag_id])
+    tags.append([x, y, z, z_rot, y_rot, tag_id])
 
 data = dict(
     tags = [
@@ -92,9 +56,9 @@ for tag in tags:
     pose = d['pose']
     pose['position'] = dict(x=tag[0], y=tag[1], z=tag[2])
     
-    print(f"using angles X {X_angle_offset} Y {tag[-2]+270} Z 0")
+    print(f"using angles X {X_angle_offset} Y {tag[-2]+270} Z {tag[3]}")
 
-    r = Rotation.from_euler("XY", (X_angle_offset, tag[-2]+90), degrees=True)
+    r = Rotation.from_euler("XYZ", (X_angle_offset, tag[-2]+90, tag[3]), degrees=True)
     vec = r.as_rotvec()
     
     pose['rotation'] = dict(x=float(vec[0]), y=float(vec[1]), z=float(vec[2]))
