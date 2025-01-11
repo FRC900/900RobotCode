2f7aa40cf957b1aafbde1fbeed48b556ae141b49
EliasA0123 <elias.adali@gmail.com>
2025-01-11 16:18:59 -0500

    progress on tf(?)

---
+++ zebROS_ws/src/behaviors/src/2025_reef_tf.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import rospy
+import geometry_msgs
+from behavior_actions.msg import AutoAlignReef
+import tf2_ros
+
+rospy.init_node('reef_transforms', anonymous=True) #Initializes the node
+
+x_offset = rospy.get_param("reef_x_transform")
+y_offset = rospy.get_param("reef_y_transform")
+
+broadcaster = tf2_ros.StaticTransformBroadcaster() #defines the broadcaster
+t = geometry_msgs.msg.TransformStamped()
+t.header.stamp = rospy.Time.now()
+t.header.frame_id = "blue_map"
+t.child_frame_id = "red_map"
+t.transform.translation.x = x_offset
+t.transform.translation.y = y_offset
+
+t.transform.translation.z = 0
+t.transform.rotation.x = 0
+t.transform.rotation.y = 0
+t.transform.rotation.z = 0
+t.transform.rotation.w = 0
+
+broadcaster.sendTransform(t)
+
+def callback(msg: AutoAlignReef):
+    if msg.pipe == msg.LEFT_PIPE:
+        t.transform.translation.y = -y_offset
+    else:
+        t.transform.translation.y = y_offset
+
+    broadcaster.sendTransform(t)
+
+rospy.Subscriber('/align_to_reef/pipe', AutoAlignReef, callback) #subscribes to MatchSpecificData
+rospy.spin()
