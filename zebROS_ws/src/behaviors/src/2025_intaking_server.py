93c3e4cb2ea6a0f68f48fdd0dbe57f6eccd5ae02
Torchtopher <torchtopher@gmail.com>
2025-02-03 18:19:47 -0500

    intaking

--- zebROS_ws/src/behaviors/src/2025_intaking_server.py
+++ zebROS_ws/src/behaviors/src/2025_intaking_server.py
@@ -12,52 +12,73 @@ from talon_controller_msgs.srv import Command, CommandRequest
 
 class IntakingServer(object):
     def __init__(self, name):
-        self.__action_name = name
         self.result = Intaking2025Result()
         self.goal = Intaking2025Goal()
         self.feedback = Intaking2025Feedback()
-        self.elevater_client = actionlib.SimpleActionClient('Elevater', Elevater2025Action)
+        self.elevater_client = actionlib.SimpleActionClient('ADD_CORRECT_PATH', Elevater2025Action)
         rospy.loginfo('Waiting for Elevater action server')
         self.elevater_client.wait_for_server()
-        self.roller_client = actionlib.SimpleActionClient('Roller', Roller2025Action)
+        self.roller_client = actionlib.SimpleActionClient('ADD_CORRECT_PATH', Roller2025Action)
         rospy.loginfo('Waiting for Roller action server')
 
-        self.intake_client = rospy.ServiceProxy(f"/frcrobot_jetson/{rospy.get_param('controller_name')}/command", Command)
+        self.intake_service = rospy.ServiceProxy(f"/frcrobot_jetson/{rospy.get_param('controller_name')}/command", Command)
         
+        self.server = actionlib.SimpleActionServer(self.action_name, Intaking2025Goal, execute_cb=self.execute_cb, auto_start = False)
+        self.server.start()
+        rospy.loginfo("Intaking actionlib server")
 
     def execute_cb(self, goal : Intaking2025Goal):
         self.feedback.state = self.feedback.NOT_ACQUIRED
         self.server.publish_feedback(self.feedback)
-        elevater_result = Elevater2025Result()
 
         elevater_goal = Elevater2025Goal()
-        elevater_goal.mode = 
+        elevater_goal.mode = Elevater2025Goal.INTAKE
+        
+        elevator_done = False
         def elevater_done_cb(state, result):
+            nonlocal elevator_done
+            elevator_done = True
             rospy.loginfo('Elevater action finished')
+
         self.elevater_client.send_goal(elevater_goal, elevater_done_cb)
+        rospy.loginfo("Intaking server - Sent elevator goal")
+        r = rospy.Rate(50.0)
+
+        while not elevator_done:
+            if self.server.is_preempt_requested():
+                rospy.loginfo("2025_intaker_server: preempted")
+                self.server.set_preempted()
+                return
+            r.sleep()
 
         rospy.loginfo('Elevater action passed check, now intaking')
 
-        intaker_goal = Intaker2025Goal()
-        def intaker_done_cb(state, result):
-            rospy.loginfo('Intaker action finished')
-
-        self.intaker_client.send_goal(intaker_goal)
-        rospy.loginfo('Intaker action sent')
-        self.feedback.state = self.feedback.IN_INTAKE
-        self.server.publish_feedback(self.feedback)
         roller_goal = Roller2025Goal()
-        roller_goal.mode = 0 # Set the roller goal to INTAKE
+        roller_goal.mode = Roller2025Goal.INTAKE
+
+        roller_done = False
         def roller_done_cb(state, result):
-            rospy.loginfo('Roller action finished')
-        self.roller_client.send_goal(roller_goal.mode, roller_done_cb)
-        rospy.loginfo('Roller action sent')
+            nonlocal roller_done
+            roller_done = True
+            rospy.loginfo('Intaking - Roller action finished')
+        
+        self.roller_client.send_goal(roller_goal, roller_done_cb)
+        self.intake_service.call(Command(1.0))
+
+        while not roller_done:
+            if self.server.is_preempt_requested():
+                rospy.loginfo("2025_intaker_server: preempted")
+                self.server.set_preempted()
+                return
+            r.sleep()
+
+        rospy.loginfo('Intaking - Roller action sent')
         self.feedback.state = self.feedback.IN_ROLLER
         self.server.publish_feedback(self.feedback)
         self.result.success = True
         rospy.loginfo("2025_intaking_server: succeeded")
         self.server.set_succeeded(self.result) 
-
+        
 
 if __name__ == '__main__':
     rospy.init_node('2025_intaking_server')
