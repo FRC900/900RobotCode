f9fecf78456ca85344fa31c31ccb27f10017f05f
Torchtopher <torchtopher@gmail.com>
2025-02-03 18:02:06 -0500

    intaker remove file

--- zebROS_ws/src/behaviors/src/2025_intaking_server.py
+++ zebROS_ws/src/behaviors/src/2025_intaking_server.py
@@ -7,6 +7,8 @@ from behavior_actions.msg import Intaking2025Goal, Intaking2025Feedback, Intakin
 from behavior_actions.msg import Elevater2025Goal, Elevater2025Feedback, Elevater2025Result, Elevater2025Action
 from behavior_actions.msg import Roller2025Goal, Roller2025Feedback, Roller2025Result, Roller2025Action
 from behavior_actions.msg import Intaker2025Goal, Intaker2025Feedback, Intaker2025Result, Intaker2025Action
+from talon_controller_msgs.srv import Command, CommandRequest
+
 
 class IntakingServer(object):
     def __init__(self, name):
@@ -19,26 +21,27 @@ class IntakingServer(object):
         self.elevater_client.wait_for_server()
         self.roller_client = actionlib.SimpleActionClient('Roller', Roller2025Action)
         rospy.loginfo('Waiting for Roller action server')
-        self.roller_client.wait_for_server()
-        self.intaker_client = actionlib.SimpleActionClient('Intaker', Intaker2025Action)
-        rospy.loginfo('Waiting for Intaker action server')
-        self.intaker_client.wait_for_server()
 
+        self.intake_client = rospy.ServiceProxy(f"/frcrobot_jetson/{rospy.get_param('controller_name')}/command", Command)
+        
 
     def execute_cb(self, goal : Intaking2025Goal):
-        elevater_result = Elevater2025Result()
         self.feedback.state = self.feedback.NOT_ACQUIRED
         self.server.publish_feedback(self.feedback)
-        if not elevater_result.success:
-            elevater_goal = Elevater2025Goal()
-            elevater_goal.mode = 4
-            def elevater_done_cb(state, result):
-                rospy.loginfo('Elevater action finished')
-            self.elevater_client.send_goal(elevater_goal.mode, elevater_done_cb)
+        elevater_result = Elevater2025Result()
+
+        elevater_goal = Elevater2025Goal()
+        elevater_goal.mode = 
+        def elevater_done_cb(state, result):
+            rospy.loginfo('Elevater action finished')
+        self.elevater_client.send_goal(elevater_goal, elevater_done_cb)
+
         rospy.loginfo('Elevater action passed check, now intaking')
+
         intaker_goal = Intaker2025Goal()
         def intaker_done_cb(state, result):
             rospy.loginfo('Intaker action finished')
+
         self.intaker_client.send_goal(intaker_goal)
         rospy.loginfo('Intaker action sent')
         self.feedback.state = self.feedback.IN_INTAKE
