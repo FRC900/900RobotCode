53e0eb5ef7fbd9f79d1a2e5262b66d1d2bf97be2
BenBean18 <beakerthespeaker@gmail.com>
2025-03-11 00:04:33 -0400

    Add option for fully manual alliance color override

--- zebROS_ws/src/behaviors/src/tagslam_match_data_republisher.py
+++ zebROS_ws/src/behaviors/src/tagslam_match_data_republisher.py
@@ -2,6 +2,8 @@
 import rospy
 from nav_msgs.msg import Odometry
 from frc_msgs.msg import MatchSpecificData
+from std_srvs.srv import SetBool, SetBoolRequest, SetBoolResponse
+from behavior_actions.srv import OverrideAllianceColor, OverrideAllianceColorRequest, OverrideAllianceColorResponse
 
 FIELD_LENGTH = 17.55
 
@@ -10,11 +12,31 @@ class TagSLAMZeroAndRepublish:
     tagslam_last_x = None
     tagslam_last_time = None
     robot_enabled = True
+    manual_override_enabled = False
+    manual_alliance = MatchSpecificData.ALLIANCE_COLOR_UNKNOWN
 
     def __init__(self):
         self.tagslam_sub = rospy.Subscriber("/tagslam/odom/body_frc_robot", Odometry, callback=self.tagslam_cb, tcp_nodelay=True, queue_size=1)
         self.match_data_sub = rospy.Subscriber("/frcrobot_rio/match_data_raw", MatchSpecificData, callback=self.match_data_cb, tcp_nodelay=True, queue_size=1)
         self.match_data_pub = rospy.Publisher("/frcrobot_rio/match_data", MatchSpecificData, tcp_nodelay=True, queue_size=1)
+        self.override_srv = rospy.Service("enable_manual_override", SetBool, self.manual_override_cb)
+        self.set_alliance_srv = rospy.Service("set_alliance_color", OverrideAllianceColor, self.set_alliance_cb)
+    
+    def manual_override_cb(self, req: SetBoolRequest):
+        if req.data:
+            rospy.logwarn("tagslam_match_data_republisher: ENABLING MANUAL OVERRIDE. HERE BE DRAGONS. (or unicorns?)")
+            rospy.logwarn(f"tagslam_match_data_republisher: MANUAL OVERRIDE ENABLED, ALLIANCE COLOR IS CURRENTLY {self.alliance_color_to_string(self.manual_alliance)}")
+            self.manual_override_enabled = True
+        else:
+            rospy.logwarn("tagslam_match_data_republisher: disabling manual override")
+            self.manual_override_enabled = False
+        return SetBoolResponse()
+    
+    def set_alliance_cb(self, req: OverrideAllianceColorRequest):
+        self.manual_alliance = req.allianceColor
+        if self.manual_override_enabled:
+            rospy.logwarn(f"tagslam_match_data_republisher: MANUAL OVERRIDE ENABLED, ALLIANCE COLOR SET TO {self.alliance_color_to_string(self.manual_alliance)}")
+        return OverrideAllianceColorResponse()
 
     def tagslam_cb(self, msg: Odometry):
         if not self.robot_enabled:
@@ -41,11 +63,17 @@ class TagSLAMZeroAndRepublish:
     
     def match_data_cb(self, msg: MatchSpecificData):
         self.robot_enabled = msg.Enabled
-        if self.tagslam_alliance != MatchSpecificData.ALLIANCE_COLOR_UNKNOWN:
+
+        if self.manual_override_enabled and self.manual_alliance != MatchSpecificData.ALLIANCE_COLOR_UNKNOWN:
+            prev_alliance = msg.allianceColor
+            if self.manual_alliance != prev_alliance or self.manual_alliance != self.tagslam_alliance:
+                rospy.logerr(f"tagslam_match_data_republisher: ***MANUAL OVERRIDE*** FMS alliance color = {self.alliance_color_to_string(prev_alliance)}, TagSLAM alliance color = {self.alliance_color_to_string(self.tagslam_alliance)}. One of these DOES NOT MATCH MANUAL OVERRIDE = {self.alliance_color_to_string(self.manual_alliance)}. Using MANUAL color {self.alliance_color_to_string(self.manual_alliance)}.")
+                msg.allianceColor = self.manual_alliance
+        elif self.tagslam_alliance != MatchSpecificData.ALLIANCE_COLOR_UNKNOWN:
             prev_alliance = msg.allianceColor
             if self.tagslam_alliance != prev_alliance:
-                rospy.logerr(f"FMS alliance color = {self.alliance_color_to_string(prev_alliance)}. This DOES NOT MATCH TagSLAM-based color = {self.alliance_color_to_string(self.tagslam_alliance)}, based on x = {self.tagslam_last_x} at {self.tagslam_last_time}. Using TagSLAM color {self.alliance_color_to_string(self.tagslam_alliance)}.")
-                msg.allianceColor = self.tagslam_alliance
+                rospy.logerr(f"tagslam_match_data_republisher: FMS alliance color = {self.alliance_color_to_string(prev_alliance)}. This DOES NOT MATCH TagSLAM-based color = {self.alliance_color_to_string(self.tagslam_alliance)}, based on x = {self.tagslam_last_x} at {self.tagslam_last_time}. Using TagSLAM color {self.alliance_color_to_string(self.tagslam_alliance)}.")
+                msg.allianceColor = self.tagslam_alliance 
         self.match_data_pub.publish(msg)
  
 if __name__ == "__main__":
