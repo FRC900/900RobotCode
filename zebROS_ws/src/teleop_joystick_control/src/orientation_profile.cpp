486ff88c4067256930fed07da25db3b275efae17
Kevin Jaget <kjaget@nc.rr.com>
2025-03-20 17:11:45 -0400

    Add config items and ddr for profile max speed, accel, jerk Minor fixes for normalizing angles

--- zebROS_ws/src/teleop_joystick_control/src/orientation_profile.cpp
+++ zebROS_ws/src/teleop_joystick_control/src/orientation_profile.cpp
@@ -2,37 +2,51 @@
 #include "ros/console.h"
 #include "teleop_joystick_control/orientation_profile.h"
 
-OrientationProfile::OrientationProfile()
+OrientationProfile::OrientationProfile(ros::NodeHandle &nh)
+    : ddr_{ros::NodeHandle(nh, "orientation_profile")}
 {
-    // TODO : params from config for max velocity, acceleration and jerk
-    ruckig_input_.max_velocity[0] = 6;
-    ruckig_input_.max_acceleration[0] = 18;
-    ruckig_input_.max_jerk[0] = 54;
+    // Feels dumb to repeat this twice (once for DDR, once here), but
+    // the other choice is to make a NodeHandle class member var which
+    // is only used in the constructor.
+	ros::NodeHandle n_params(nh, "orientation_profile");
+    n_params.param<double>("velocity", ruckig_input_.max_velocity[0], 6.0);
+    n_params.param<double>("acceleration", ruckig_input_.max_acceleration[0], 12.0);
+    n_params.param<double>("jerk", ruckig_input_.max_jerk[0], 64.0);
+
+    ddr_.registerVariable<double>("velocity", &ruckig_input_.max_velocity[0], "Orientation profile velocity", 0.0, 20.0);
+    ddr_.registerVariable<double>("acceleration", &ruckig_input_.max_acceleration[0], "Orientation profile acceleration", 0.0, 200.0);
+    ddr_.registerVariable<double>("jerk", &ruckig_input_.max_jerk[0], "Orientation profile jerk", 0.0, 1000.0);
+
+    ddr_.publishServicesTopics();
 }
 
 bool OrientationProfile::createProfile(const OrientationState &current_state, const double target_orientation)
 {
     // Prevent constantly regenrating the same trajectory
-    if (most_recent_was_trajectory_ && (target_orientation == target_state_.position))
+    const auto normalized_taget_orientation = angles::normalize_angle(target_orientation);
+    if (most_recent_was_trajectory_ && (normalized_taget_orientation == target_state_.position))
     {
         return true;
     }
     ROS_WARN_STREAM("Generating profile for angle " << target_orientation);
     initial_state_ = OrientationState(angles::normalize_angle(current_state.position), current_state.velocity, current_state.acceleration);
-    target_state_ = OrientationState(angles::normalize_angle(target_orientation), 0, 0);
+    target_state_ = OrientationState(normalized_taget_orientation, 0, 0);
     start_time_ = ros::Time::now();
 
-    // Profile is delta angle from initial state position
     ruckig_input_.current_position[0] = 0;
     ruckig_input_.current_velocity[0] = current_state.velocity;
     ruckig_input_.current_acceleration[0] = current_state.acceleration;
-    ruckig_input_.target_position[0] = angles::shortest_angular_distance(current_state.position, target_orientation);
+    // Profile is delta angle from initial state position. The profile goes from 0
+    // to delta angle in the nearest driection.  The code which returns the angle
+    // to drive to is responsible for adding the initial state position back in.
+    ruckig_input_.target_position[0] = angles::shortest_angular_distance(current_state.position, normalized_taget_orientation);
     ruckig_input_.target_velocity[0] = 0;
     ruckig_input_.target_acceleration[0] = 0;
     const auto result = ruckig_obj_.calculate(ruckig_input_, ruckig_trajectory_);
     if (result != ruckig::Result::Working)
     {
         ROS_ERROR_STREAM("Failed to create trajectory: " << static_cast<int>(result));
+        trajectory_running_ = false;
         return false;
     }
     trajectory_running_ = true;
