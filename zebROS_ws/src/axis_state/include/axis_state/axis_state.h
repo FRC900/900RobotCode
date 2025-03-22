4a68311994656c3055e2bebb5f772032637336ed
Torchtopher <torchtopher@gmail.com>
2025-03-22 14:28:52 -0400

    revert the teleop changes

--- zebROS_ws/src/axis_state/include/axis_state/axis_state.h
+++ zebROS_ws/src/axis_state/include/axis_state/axis_state.h
@@ -5,7 +5,7 @@
 #include "std_msgs/Bool.h"
 #include "std_msgs/Float64.h"
 #include "std_msgs/Float64MultiArray.h"
-#include "teleop_orientation_msgs/TeleopOrientation.h"
+#include "pid_velocity_msg/PIDVelocity.h"
 
 class AlignActionAxisConfig
 {
@@ -96,7 +96,7 @@ class AlignActionAxisStatePosition : public AlignActionAxisState<std_msgs::Float
 };
 
 // These messages are sent as (position, velocity)
-class AlignActionAxisStatePositionVelocity : public AlignActionAxisState<teleop_orientation_msgs::TeleopOrientation> {
+class AlignActionAxisStatePositionVelocity : public AlignActionAxisState<pid_velocity_msg::PIDVelocity> {
 	public:
 		AlignActionAxisStatePositionVelocity(ros::NodeHandle &nh,
 							const std::string &enable_pub_topic,
@@ -107,10 +107,9 @@ class AlignActionAxisStatePositionVelocity : public AlignActionAxisState<teleop_
 			// Set defaults for PID node topics to prevent
 			// spam of "Waiting for first setpoint message."
 
-			teleop_orientation_msgs::TeleopOrientation position_velocity_msg;
+			pid_velocity_msg::PIDVelocity position_velocity_msg;
 			position_velocity_msg.position = 0.0;
 			position_velocity_msg.velocity = 0.0;
-			position_velocity_msg.drive_from_teleop = false;
 			command_pub_.publish(position_velocity_msg);
 
 			std_msgs::Float64 position_msg;
@@ -120,10 +119,9 @@ class AlignActionAxisStatePositionVelocity : public AlignActionAxisState<teleop_
 
 		void setCommand(double position_command, double velocity_command)
 		{
-			teleop_orientation_msgs::TeleopOrientation command_msg;
+			pid_velocity_msg::PIDVelocity command_msg;
 			command_msg.position = position_command;
 			command_msg.velocity = velocity_command;
-			command_msg.drive_from_teleop = false;
 			command_pub_.publish(command_msg);
 		}
 };
