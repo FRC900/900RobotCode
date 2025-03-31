b28526654e59e68628640e9cdee2446a082424c4
Kevin Jaget <kjaget@nc.rr.com>
2025-03-31 06:39:31 -0400

    More mrcal updates

--- zebROS_ws/src/rosbag_scripts/scripts/mrcal.sh
+++ zebROS_ws/src/rosbag_scripts/scripts/mrcal.sh
@@ -8,7 +8,7 @@ rosrun image_view extract_images image:=/ov2311_10_9_0_9_video0/image_raw
 # This could be done for live cameras as well
 
 # Generate corner data from charuco board
-# If you can find a pure chessboard target, there's mrgingham as described here
+# If you can find a pure chessboard target without arucotags, there's mrgingham as described here
 #  https://mrcal.secretsauce.net/how-to-calibrate.html#org524f742
 python3 /home/ubuntu/900RobotCode/zebROS_ws/src/rosbag_scripts/scripts/charuco_calibrator.py  | tee corners.vnl
 
@@ -18,9 +18,10 @@ python3 /home/ubuntu/900RobotCode/zebROS_ws/src/rosbag_scripts/scripts/charuco_c
   feedgnuplot --domain --square --set 'xrange [0:2000] noextend' --set 'yrange [2000:0] noextend'
 
 # Run calibration
+# fov_x_deg is the horizontal field of view of the camera, this is a guess which can be refined later
 mrcal-calibrate-cameras                                                         \
   --corners-cache corners.vnl                                                   \
-  --lensmodel LENSMODEL_SPLINED_STEREOGRAPHIC_order=3_Nx=30_Ny=18_fov_x_deg=150 \
+  --lensmodel LENSMODEL_SPLINED_STEREOGRAPHIC_order=3_Nx=30_Ny=25_fov_x_deg=80  \
   --focal 1000                                                                  \
   --object-spacing 0.0254                                                       \
   --object-width-n 7                                                            \
