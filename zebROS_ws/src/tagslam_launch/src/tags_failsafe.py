c61f445367883ac25d16e25db819017c5ddfcb55
giragon6 <emmemcdonald66@gmail.com>
2025-04-02 18:01:19 -0400

    add parameters and recursion prevention

--- zebROS_ws/src/tagslam_launch/src/tags_failsafe.py
+++ zebROS_ws/src/tagslam_launch/src/tags_failsafe.py
@@ -3,16 +3,19 @@
 import rospy
 from apriltag_msgs.msg import ApriltagArrayStamped
 
-tolerance = 500 # TODO: Get this from params
+tolerance = rospy.get_param("tolerance", 0.5) # seconds
+rate = rospy.get_param("rate", 100) # Hz
 
 def tag_detection_cb(msg: ApriltagArrayStamped, camera):
-    cameras_last_sent[camera] = rospy.Time.now()
+    if camera.frameid != f"fake_{camera}":
+        cameras_last_sent[camera] = rospy.Time.now()
     
-cameras = ["ov2311_10_9_0_10_video0", "ov2311_10_9_0_10_video1", "ov2311_10_9_0_9_video0", "ov2311_10_9_0_9_video1"] # TODO: Get this from params 
-cameras_last_sent = {}
-camera_pubs = {}
+cameras = rospy.get_param("camera_names", ["ov2311_10_9_0_10_video0", "ov2311_10_9_0_10_video1", "ov2311_10_9_0_9_video0", "ov2311_10_9_0_9_video1"])
+cameras_last_sent: dict[str, int] = {}
+camera_pubs: dict[str, rospy.Publisher] = {}
 
 if __name__ == '__main__':
+    r = rospy.Rate(rate)
     rospy.init_node('tags_failsafe_node')
 
     for camera in cameras:
@@ -25,4 +28,6 @@ if __name__ == '__main__':
             rospy.logwarn_throttle(1.0, f'tags_failsafe_node: camera "{camera}" offline! publishing empty tag array')
             msg = ApriltagArrayStamped()
             msg.tags = []
+            msg.frameid = f"fake_{camera}"
             camera_pubs[camera].publish(msg)
+        r.sleep()
