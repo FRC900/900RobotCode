e9eea582d0d594ce4997cd52e04c81cd6e792411
BenBean18 <beakerthespeaker@gmail.com>
2025-04-18 13:16:22 -0400

    Swap the physical cameras .10 video1 and .9 video0, this updates intrinsics to match

--- zebROS_ws/src/rosbag_scripts/scripts/tagslam_latency.py
+++ zebROS_ws/src/rosbag_scripts/scripts/tagslam_latency.py
@@ -12,7 +12,8 @@ tags_9_1 = []
 tags_10_1 = []
 vels = []
 ang_vels = []
-for topic, msg, t in rosbag.Bag(sys.argv[1]).read_messages(["/tagslam/odom/body_frc_robot", "/apriltag_detection_ov2311_10_9_0_9_video0/tags", "/apriltag_detection_ov2311_10_9_0_9_video1/tags", "/apriltag_detection_ov2311_10_9_0_10_video0/tags", "/apriltag_detection_ov2311_10_9_0_10_video1/tags", "/frcrobot_jetson/swerve_drive_controller/cmd_vel_out"]):
+enableds = []
+for topic, msg, t in rosbag.Bag(sys.argv[1]).read_messages(["/tagslam/odom/body_frc_robot", "/apriltag_detection_ov2311_10_9_0_9_video0/tags", "/apriltag_detection_ov2311_10_9_0_9_video1/tags", "/apriltag_detection_ov2311_10_9_0_10_video0/tags", "/apriltag_detection_ov2311_10_9_0_10_video1/tags", "/frcrobot_jetson/swerve_drive_controller/cmd_vel_out", "/frcrobot_rio/match_data"]):
     if topic == "/tagslam/odom/body_frc_robot":
         times.append(t.to_sec())
         latencies.append(t.to_sec() - msg.header.stamp.to_sec())
@@ -27,14 +28,17 @@ for topic, msg, t in rosbag.Bag(sys.argv[1]).read_messages(["/tagslam/odom/body_
     if topic == "/frcrobot_jetson/swerve_drive_controller/cmd_vel_out":
         vels.append((t.to_sec(), math.hypot(msg.twist.linear.x, msg.twist.linear.y)))
         ang_vels.append((t.to_sec(), abs(msg.twist.angular.z)))
+    if topic == "/frcrobot_rio/match_data":
+        enableds.append((t.to_sec(), 1 if msg.Enabled else 0))
 
 plt.plot(times, latencies, '-', color="black", label="latencies")
-plt.plot(*zip(*tags_9_0), '-', color="green", label="tags9_0")
-plt.plot(*zip(*tags_9_1), '-', color="blue", label="tags9_1")
-plt.plot(*zip(*tags_10_0), '-', color="red", label="tags10_0")
-plt.plot(*zip(*tags_10_1), '-', color="yellow", label="tags10_1")
-plt.plot(*zip(*vels), '-', color="pink", label="vel")
-plt.plot(*zip(*ang_vels), '-', color="purple", label="ang_vel")
+plt.plot(*zip(*enableds), '-', color="green", label="enableds")
+# plt.plot(*zip(*tags_9_0), '-', color="green", label="tags9_0")
+# plt.plot(*zip(*tags_9_1), '-', color="blue", label="tags9_1")
+# plt.plot(*zip(*tags_10_0), '-', color="red", label="tags10_0")
+# plt.plot(*zip(*tags_10_1), '-', color="yellow", label="tags10_1")
+# plt.plot(*zip(*vels), '-', color="pink", label="vel")
+# plt.plot(*zip(*ang_vels), '-', color="purple", label="ang_vel")
 plt.legend()
 plt.show()
 
